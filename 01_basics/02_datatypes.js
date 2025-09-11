a32ae6cf2525a8ad34e5136ee54b75cee3283d81
Tanishq Shrivas <118658128+tanishq-sh17@users.noreply.github.com>
2025-09-11 20:54:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"  // treat all JS code as newer version
+
+// alert("hello")   // we are using nodeJS, not browser
+
+console.log(3+3)  // code readability should be high
+console.log("Tanishq")
+
+let name = "TAnishq"
+let age = 21
+let isLoggedIn = false
+
+// Different datatypes
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "tan")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
