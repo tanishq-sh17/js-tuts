4940ca8a52c7590abbccae7cea1ba36ab04eaf01
Tanishq Shrivas <118658128+tanishq-sh17@users.noreply.github.com>
2025-09-12 08:50:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,36 @@
+// let score  = "33abc"
+// let score  = null
+// let score = undefined
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+
+// let loggedIn = 1
+// let loggedIn = 0
+// let loggedIn = ""
+let loggedIn = "Tanishq"
+let booleanIdLoggedIn = Boolean(loggedIn)
+// console.log(booleanIdLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Tanishq" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
