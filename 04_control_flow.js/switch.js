d242e3a4e8f48149113247a97c9081bc3f4c9d3b
Tanishq Shrivas <118658128+tanishq-sh17@users.noreply.github.com>
2025-10-04 14:29:32 +0000

    control_flow_switch.js

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+                               // syntax of switch statement
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feburary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console("default case matched")
+        break;
+}
